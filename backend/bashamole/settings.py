fbe7527ea5201fbf9c3c7711c3a4b2d21203ffab
mfwolffe <wolffemf@dukes.jmu.edu>
2025-06-23 21:52:15 -0400

    ports

--- backend/bashamole/settings.py
+++ backend/bashamole/settings.py
@@ -144,6 +144,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://localhost:3001",
     "http://localhost:5173",
     "https://bashamole.sh",
     "https://www.bashamole.sh",
